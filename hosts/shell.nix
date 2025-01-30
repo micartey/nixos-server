654854c0d8a1737b59955e72f770266091a99a11
micartey <me@micartey.dev>
2025-01-30 22:03:20 +0100

    update flake and remove nix-ld

--- hosts/shell.nix
+++ hosts/shell.nix
@@ -1,15 +1,8 @@
 {
   programs.zsh = {
     enable = true;
-
     enableCompletion = true;
     syntaxHighlighting.enable = true;
-    autosuggestion.enable = true;
-
-    initExtra = ''
-      bindkey "^[[1;5C" forward-word
-      bindkey "^[[1;5D" backward-word
-    '';
   };
 
   environment.pathsToLink = [ "/share/zsh" ];
